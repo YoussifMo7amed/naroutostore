7c7202a7737d624a161944d35fc5a384c47cfe53
Youssef Mohamed <joe375649@gmail.com>
2024-03-19 16:13:36 +0200

    env_create

--- lib/main.dart
+++ lib/main.dart
@@ -2,18 +2,28 @@ import 'dart:io';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:naroutoshop/core/apps/envvariables.dart';
 import 'package:naroutoshop/naroutoshop.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  await EnvVariable.instance.init(envType: EnvTypeEnum.prod);
+
   Platform.isAndroid
       ? await Firebase.initializeApp(
           options: const FirebaseOptions(
-              apiKey: "AIzaSyDRxAUz608tIpFx1DSJFUU8Hm6P9V3NqQM",
-              appId: "1:340026377526:android:3f7804e0f1786cf5c85f97",
-              messagingSenderId: "340026377526",
-              projectId: "narouto-store",
-              storageBucket: "narouto-store.appspot.com"))
+              apiKey: 'AIzaSyDRxAUz608tIpFx1DSJFUU8Hm6P9V3NqQM',
+              appId: '1:340026377526:android:3f7804e0f1786cf5c85f97',
+              messagingSenderId: '340026377526',
+              projectId: 'narouto-store',
+              storageBucket: 'narouto-store.appspot.com'))
       : await Firebase.initializeApp();
+await SystemChrome.setPreferredOrientations([
+  DeviceOrientation.portraitUp,
+  DeviceOrientation.portraitDown,
+]).then((_) {
   runApp(const NaroutoStore());
+});
 }
--- lib/naroutoshop.dart
+++ lib/naroutoshop.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:naroutoshop/core/apps/envvariables.dart';
 
 class NaroutoStore extends StatelessWidget {
   const NaroutoStore({super.key});
@@ -7,7 +8,7 @@ class NaroutoStore extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-     
+     debugShowCheckedModeBanner: EnvVariable.instance.debugShowMode,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
