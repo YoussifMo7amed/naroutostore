55914c709f2b9585e4a905ae58c08d5a2668c0b8
Youssef Mohamed <joe375649@gmail.com>
2024-03-20 14:31:44 +0200

    font_family and weight is done.

---
+++ lib/core/styles/fonts/font_family_helper.dart
@@ -0,0 +1,4 @@
+class FontFamilyHelper {
+  static const String cairo = 'Cairo';
+  static const String poppins = 'Poppins';
+}
--- lib/naroutoshop.dart
+++ lib/naroutoshop.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:naroutoshop/core/apps/connectivity_controller.dart';
 import 'package:naroutoshop/core/apps/envvariables.dart';
 import 'package:naroutoshop/core/common/screens/no_network_screen.dart';
+import 'package:naroutoshop/core/styles/fonts/font_family_helper.dart';
+import 'package:naroutoshop/core/styles/fonts/font_wieght_helper.dart';
 
 class NaroutoStore extends StatelessWidget {
   const NaroutoStore({super.key});
@@ -10,43 +13,82 @@ class NaroutoStore extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(valueListenable: ConnectivityController.instance.isConnected, builder: 
-    (_, value, __) {
-      if (value) {
-         return ScreenUtilInit(
-           designSize: const Size(375, 812),
-           child: MaterialApp(
-                 title: 'Narouto Shop',
-                 debugShowCheckedModeBanner: EnvVariable.instance.debugShowMode,
-                 theme: ThemeData(
-                   colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-                   useMaterial3: true,
-                   
-                 ),
-                 builder: (context, Widget) {
-                   return Scaffold(
-            body: Builder(builder: (context) {
-               ConnectivityController.instance.init();
-               return Widget!;
-            },),
-                   );
-                 },
-                 home: Scaffold(),
-               ),
-         );
-      } else {
-     return    MaterialApp(
-      title: 'Narouto Shop',
-      debugShowCheckedModeBanner: EnvVariable.instance.debugShowMode,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home:const NoNetworkScreen(),
+    return ValueListenableBuilder(
+      valueListenable: ConnectivityController.instance.isConnected,
+      builder: (_, value, __) {
+        if (value) {
+          return ScreenUtilInit(
+            designSize: const Size(375, 812),
+            child: MaterialApp(
+              title: 'Narouto Shop',
+              debugShowCheckedModeBanner: EnvVariable.instance.debugShowMode,
+              theme: ThemeData(
+                colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+                useMaterial3: true,
+              ),
+              builder: (context, Widget) {
+                return Scaffold(
+                  body: Builder(
+                    builder: (context) {
+                      ConnectivityController.instance.init();
+                      return Widget!;
+                    },
+                  ),
+                );
+              },
+              home: const Scaffold(
+                body: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'hello',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'اهلا',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'hello',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontweightHelper.bold,
+                          fontFamily: FontFamilyHelper.poppins,
+                        ),
+                      ),
+                      Text(
+                        'اهلا',
+                        style: TextStyle(
+                          fontWeight: FontweightHelper.bold,
+                          fontFamily: FontFamilyHelper.cairo,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          );
+        } else {
+          return MaterialApp(
+            title: 'Narouto Shop',
+            debugShowCheckedModeBanner: EnvVariable.instance.debugShowMode,
+            theme: ThemeData(
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+              useMaterial3: true,
+            ),
+            home: const NoNetworkScreen(),
+          );
+        }
+      },
     );
-      }
-    },
-    );
-  
   }
 }
