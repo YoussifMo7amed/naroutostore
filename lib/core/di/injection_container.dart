a3986e33ab46de5ae41e7e9a5980e8aacd6b3947
Youssef Mohamed <joe375649@gmail.com>
2024-04-22 21:35:36 +0200

    dio_factory done.

--- lib/core/di/injection_container.dart
+++ lib/core/di/injection_container.dart
@@ -1,7 +1,7 @@
 import 'package:get_it/get_it.dart';
-//import 'package:naroutoshop/core/apps/app_cubit/app_cubit.dart';
-//import 'package:naroutoshop/core/service/graphql/api_service.dart';
-//import 'package:naroutoshop/core/service/graphql/dio_factory.dart';
+import 'package:naroutoshop/core/apps/app_cubit/app_cubit.dart';
+import 'package:naroutoshop/core/service/graphql/api_service.dart';
+import 'package:naroutoshop/core/service/graphql/dio_factory.dart';
 
 final s1 = GetIt.instance;
 
@@ -10,8 +10,8 @@ Future<void> setupInjector() async {
 }
 
 Future<void> _initCore() async {
- // final dio = DioFactory.getDio();
- // s1
-   // ..registerSingleton(AppCubit())
-    //..registerLazySingleton<ApiService>(() => ApiService(dio));
+  final dio = DioFactory.getDio();
+  s1
+    ..registerSingleton(AppCubit())
+    ..registerLazySingleton<ApiService>(() => ApiService(dio));
 }
